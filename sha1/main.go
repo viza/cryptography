2e521f93b2059f2c181909b0b57d842b5a2a8ce2
Vitaliy Zayets <vitaliy.zayets@gmail.com>
2023-06-07 15:09:02 +0300

    Add version of sha1 from file

--- sha1/main.go
+++ sha1/main.go
@@ -4,20 +4,63 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
 
-	str := "This is a sha1 test string"
+	printHelp()
 
-	SHA1FromStr(str)
+	args := os.Args
 
+	if len(args) > 1 && args[1] != "" {
+
+		SHA1FromFile(args[1])
+	} else {
+		str := "This is a sha1 test string"
+		sha1 := SHA1FromStr(str)
+		fmt.Printf("%s: %s\n", str, sha1)
+	}
 }
 
-func SHA1FromStr(s string) {
+func SHA1FromStr(s string) string {
 	h := sha1.New()
 	h.Write([]byte(s))
 	sha1_hash := hex.EncodeToString(h.Sum(nil))
 
-	fmt.Printf("%s: %s\n", s, sha1_hash)
+	return sha1_hash
+}
+
+func SHA1FromFile(filePath string) (string, error) {
+
+	var sha1Str string
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return sha1Str, err
+	}
+	defer file.Close()
+
+	hash := sha1.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return sha1Str, err
+	}
+
+	//Get the 20 bytes hash
+	hashInBytes := hash.Sum(nil)[:20]
+
+	//Convert the bytes to a string
+	sha1Str = hex.EncodeToString(hashInBytes)
+
+	fmt.Printf("SHA1 from file: %s\n", sha1Str)
+	return sha1Str, nil
+}
+
+func printHelp() {
+	fmt.Println("Making sha1 hash...")
+	fmt.Printf("\n")
+	fmt.Println("Make sha1 from test string - use no args in command line")
+	fmt.Println("Make sha1 from test file - use filename as argument in command line")
+	fmt.Printf("\n")
 }
